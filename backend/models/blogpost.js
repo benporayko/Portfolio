58f030bff52197af49811e90d805cf6bc5296fce
bpSAIT <78457540+bpSAIT@users.noreply.github.com>
2023-05-12 07:09:32 -0600

    Changed Syntax in Model Files

--- backend/models/blogpost.js
+++ backend/models/blogpost.js
@@ -1,4 +1,5 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 // const mongoose = require('mongoose');
 // const { Schema } = mongoose;
 
